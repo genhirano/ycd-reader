623d2f87bef761e022d396ebd07d1c1e4494da00
genhirano <genhirano@hotmail.com>
2024-11-17 21:43:17 +0900

    add test

--- tests/test.rs
+++ tests/test.rs
@@ -1,15 +1,31 @@
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
 #[cfg(test)]
 mod tests {
-    use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_get_header_size_valid() {
+
+        let file_name = "tests/ycd/1000000/Pi - Dec - Chudnovsky - 0.ycd";
+        let header_size = ycd_reader::YcdFileUtil::get_header_size(file_name);
+        assert_eq!(header_size.unwrap(), 195);
+
+        let file_name = "tests/ycd/1000000/Pi - Dec - Chudnovsky - 1.ycd";
+        let header_size = ycd_reader::YcdFileUtil::get_header_size(file_name);
+        assert_eq!(header_size.unwrap(), 195);
+
+        let file_name = "tests/ycd/1000000/Pi - Dec - Chudnovsky - 14.ycd";
+        let header_size = ycd_reader::YcdFileUtil::get_header_size(file_name);
+        assert_eq!(header_size.unwrap(), 196); // 195 + 1  桁が増えるとヘッダーが1バイト増える
+
+        // -----
+        let file_name = "tests/ycd/2000000/Pi - Dec - Chudnovsky - 0.ycd";
+        let header_size = ycd_reader::YcdFileUtil::get_header_size(file_name);
+        assert_eq!(header_size.unwrap(), 195);
+        
+        let file_name = "tests/ycd/2000000/Pi - Dec - Chudnovsky - 2.ycd";
+        let header_size = ycd_reader::YcdFileUtil::get_header_size(file_name);
+        assert_eq!(header_size.unwrap(), 195);
+
     }
+
 }
